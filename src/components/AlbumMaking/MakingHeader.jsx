b75e6889c406eed8534f20cb84700fa81a855b07
bbky323 <bbky323@kau.kr>
2024-08-07 05:06:43 +0900

    album detail 1 styling final

--- src/components/AlbumMaking/MakingHeader.jsx
+++ src/components/AlbumMaking/MakingHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HeaderContainer, LogoSection, NavSection, NamingSection, StyledIcon } from './MakingHeader.style';
+import { HeaderContainer, LogoSection, EditSection, NamingSection, StyledIcon, Button, EditContainer, EditCategory } from './MakingHeader.style';
 
 export default function MakingHeader() {
   return (
@@ -10,8 +10,21 @@ export default function MakingHeader() {
           OOO님의 앨범 제작소
         </NamingSection>
       </LogoSection>
-      <NavSection>
-      </NavSection>
+      <EditSection>
+        <EditContainer>
+          <EditCategory>임</EditCategory>
+          <EditCategory>시</EditCategory>
+          <EditCategory>편</EditCategory>
+          <EditCategory>집</EditCategory>
+          <EditCategory>에</EditCategory>
+          <EditCategory>디</EditCategory>
+          <EditCategory>터</EditCategory>
+          <EditCategory>입</EditCategory>
+          <EditCategory>니</EditCategory>
+          <EditCategory>다</EditCategory>
+        </EditContainer>
+        <Button>표지 만들기</Button>
+      </EditSection>
     </HeaderContainer>
   );
 }
--- src/components/AlbumMaking/MakingHeader.style.jsx
+++ src/components/AlbumMaking/MakingHeader.style.jsx
@@ -18,14 +18,13 @@ export const LogoSection = styled.div`
   border: 1px solid black;
 `;
 
-export const NavSection = styled.div`
+export const EditSection = styled.div`
   flex: 1;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
+  padding: 10px;
   background-color: #D9D9D9;
   border: 1px solid black;
+  position: relative;
 `;
 
 export const NamingSection = styled.div`
@@ -39,4 +38,51 @@ export const NamingSection = styled.div`
 
 export const StyledIcon = styled(Icon)`
   margin-top: 5%;
+`;
+
+export const Button = styled.button`
+  width: 10%;
+  height: 30%;
+  padding: 10px;
+  position: absolute;
+  bottom: 10%;
+  right: 1%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  border: 1px solid #000;
+  cursor: pointer;
+  border-radius: 10px;
+`;
+
+export const EditContainer = styled.div`
+  width: 40%;
+  height: 20%;
+  position: absolute;
+  top: 10%;
+  left: 1%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 20px;
+  border: 1px solid #000;
+`;
+
+export const EditCategory = styled.div`
+  width: 10%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #000;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: #f0f0f0;
+  }
+  &:active {
+    background-color: #e0e0e0;
+  }
 `;
